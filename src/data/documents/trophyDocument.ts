f1d9d5070c68de4d4861e29f03eb2fd1ee5616d9
Carlos Felipe <rezendegood120@gmail.com>
2025-12-22 12:23:35 -0300

    Remove comment from trophyDocument.ts
    Removed commented code explaining the TrophyProgress model creation.

--- src/data/documents/trophyDocument.ts
+++ src/data/documents/trophyDocument.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
 import { TrophyProgressSchema } from '../../models/schemas/trophyProgress.js';
 
-// Cria o model "TrophyProgress" usando o schema que criamos
 export const TrophyProgressModel = mongoose.model('TrophyProgress', TrophyProgressSchema);
